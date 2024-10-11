cfe9ec8c6bd81fb3da4a639cae7fb0ba1cb2a33f
Ethan Brady <git@ethanbrady.xyz>
2024-10-11 13:14:16 -0600

    readme typo fix (#1)
    * Cleaned up and removed comments
    
    * Added neofetch and bat to the packages
    
    * Added biometric authentication for sudo
    
    * Disabled mouse acceleration
    
    * Set dock to autohide
    
    * Fixed a typo from Githug -> Github

--- hosts/macbook-air/configuration.nix
+++ hosts/macbook-air/configuration.nix
@@ -1,25 +1,31 @@
 { pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ pkgs.vim
-    ];
+  environment.systemPackages = with pkgs; [
+    vim
+    neofetch
+    bat
+  ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
+  programs.zsh.enable = true;
+
+  # Allow sudo commands to be run with biometrics
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # Disable mouse acceleration
+  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = -1.0;
+
+  # Set the dock to automatically hide
+  system.defaults.dock.autohide = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
   system.stateVersion = 5;
 
   # The platform the configuration will be used on.
