abd6f35cb44f04b6ab564c24b59f993d0eeeba41
Ethan Brady <git@ethanbrady.xyz>
2024-10-11 20:51:13 -0600

    Added kitty terminal configuration

--- profiles/home/shell.nix
+++ profiles/home/shell.nix
@@ -4,6 +4,7 @@
     ../../modules/home/bat.nix
     ../../modules/home/eza.nix
     ../../modules/home/fzf.nix
+    ../../modules/home/kitty.nix
     ../../modules/home/zoxide.nix
     ../../modules/home/zsh.nix
   ];
