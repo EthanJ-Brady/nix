df5ce82b2878e4f58193e294179c66d19faff87a
Ethan Brady <git@ethanbrady.xyz>
2024-10-12 13:19:21 -0600

    Added system configuration for mohs host

--- hosts/mohs/configuration.nix
+++ hosts/mohs/configuration.nix
@@ -1,10 +1,78 @@
-{ pkgs, builtins, ... }:
 {
+  pkgs,
+  builtins,
+  ...
+}:
+{
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # SSH
   services.openssh.enable = true;
   services.openssh.extraConfig = ''
     PubkeyAuthentication yes
   '';
 
+  # Experimental features
+  nix.settings.experimental-features = [
+    "flakes"
+    "nix-command"
+  ];
+
+  # Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Locale
+  time.timeZone = "America/Denver";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define user account
+  users.users.mohs = {
+    isNormalUser = true;
+    description = "mohs";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = [ ];
+  };
+
+  # Enable automatic login for the user
+  services.getty.autologinUser = "mohs";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  # State version
+  system.stateVersion = "24.05";
+
+  # Minecraft Server
   services.minecraft = {
     enable = true;
     eula = true;
