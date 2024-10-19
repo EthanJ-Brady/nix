8c4e420f1e72ba4e00f0467f96b24ac695083f55
Ethan Brady <git@ethanbrady.xyz>
2024-10-19 12:46:13 -0600

    Added lsp

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -133,11 +133,44 @@
       ];
 
       plugins = {
+        cmp = {
+          enable = true;
+          autoEnableSources = true;
+          settings = {
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+              { name = "buffer"; }
+              { name = "luasnip"; }
+            ];
+            mapping = {
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-e>" = "cmp.mapping.close()";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            };
+          };
+        };
         indent-blankline = {
           enable = true;
           settings = {
             indent = {
               char = "│";
+            };
+          };
+        };
+        lsp = {
+          enable = true;
+          servers = {
+            ts_ls.enable = true;
+            lua_ls.enable = true;
+            rust_analyzer = {
+              enable = true;
+              installCargo = true;
+              installRustc = true;
             };
           };
         };
