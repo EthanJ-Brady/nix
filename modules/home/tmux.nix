b6c3bb773e4696b5577271db51f6b059a8b1b153
Ethan Brady <git@ethanbrady.xyz>
2024-10-23 15:00:59 -0600

    Correctly setup tmux keybinds

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -10,21 +10,51 @@
     programs = lib.mkIf config.tmux.enable {
       tmux = {
         enable = true;
-        shell = "${pkgs.zsh}/bin/zsh";
+        terminal = "screen-256color";
+        shell = "\$SHELL";
         sensibleOnTop = true;
         plugins = with pkgs; [
           {
             # This plugin allows you to interoperably navigate between tmux and nvim with ctrl + left/up/right/down or nvim directional keys
             plugin = tmuxPlugins.vim-tmux-navigator;
             extraConfig = ''
-              set -g @vim_navigator_mapping_left "C-Left C-h"
-              set -g @vim_navigator_mapping_right "C-Right C-l"
-              set -g @vim_navigator_mapping_up "C-Up C-k"
-              set -g @vim_navigator_mapping_down "C-Down C-j"
+              is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+                  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+
+              bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+              bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+              bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+              bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+
+              tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+              if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+                  "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+              if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+                  "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+              bind-key -T copy-mode-vi 'C-h' select-pane -L
+              bind-key -T copy-mode-vi 'C-j' select-pane -D
+              bind-key -T copy-mode-vi 'C-k' select-pane -U
+              bind-key -T copy-mode-vi 'C-l' select-pane -R
+              bind-key -T copy-mode-vi 'C-\' select-pane -l
+
+              bind-key -n C-Up select-pane -U
+              bind-key -n C-Down select-pane -D
+              bind-key -n C-Left select-pane -L
+              bind-key -n C-Right select-pane -R
+
+              bind-key -n 'C-Left' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+              bind-key -n 'C-Down' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+              bind-key -n 'C-Up' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+              bind-key -n 'C-Right' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
             '';
           }
           tmuxPlugins.onedark-theme
         ];
+        # At least on Mac, this line was needed to get tmux to use zsh instead of sh
+        extraConfig = ''
+          set -g default-command $SHELL
+        '';
       };
     };
   };
