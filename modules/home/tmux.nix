a7d7216697ce86b5efd4987ecc1dba219e0386df
Ethan Brady <git@ethanbrady.xyz>
2024-10-23 13:54:39 -0600

    Added a couple tmux plugins

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 {
   options = {
     tmux.enable = lib.mkEnableOption "Enable tmux along with it's corresponding configuration";
@@ -10,7 +10,21 @@
     programs = lib.mkIf config.tmux.enable {
       tmux = {
         enable = true;
+        shell = "${pkgs.zsh}/bin/zsh";
         sensibleOnTop = true;
+        plugins = with pkgs; [
+          {
+            # This plugin allows you to interoperably navigate between tmux and nvim with ctrl + left/up/right/down or nvim directional keys
+            plugin = tmuxPlugins.vim-tmux-navigator;
+            extraConfig = ''
+              set -g @vim_navigator_mapping_left "C-Left C-h"
+              set -g @vim_navigator_mapping_right "C-Right C-l"
+              set -g @vim_navigator_mapping_up "C-Up C-k"
+              set -g @vim_navigator_mapping_down "C-Down C-j"
+            '';
+          }
+          tmuxPlugins.onedark-theme
+        ];
       };
     };
   };
