ce71bd311628ceaeb8af726f9522401bb5452905
Ethan Brady <git@ethanbrady.xyz>
2024-10-31 14:52:46 -0600

    added two more search engines for ease of use in nix

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -16,6 +16,32 @@
           default = "DuckDuckGo";
           privateDefault = "DuckDuckGo";
           engines = {
+            "Home Manager Options" = {
+              urls = [{
+                template = "https://home-manager-options.extranix.com/";
+                params = [
+                  { name = "release"; value = "master"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@hopt" ];
+            };
+
+            "Nix Options" = {
+              urls = [{
+                template = "https://search.nixos.org/options";
+                params = [
+                  { name = "channel"; value = "unstable"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@nopt" ];
+            };
+
             "Nix Packages" = {
               urls = [{
                 template = "https://search.nixos.org/packages";
