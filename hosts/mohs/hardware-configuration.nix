05fce85ddfb86ed7e31a330c2d22c67ecb081a77
mohs <git@ethanbrady.xyz>
2024-11-05 14:46:55 -0700

    Pulled configuration from the new mohs computer branch

--- hosts/mohs/hardware-configuration.nix
+++ hosts/mohs/hardware-configuration.nix
@@ -8,24 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "ums_realtek" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/86132c4e-0d83-498e-b18d-b1ab9ad96057";
+    { device = "/dev/disk/by-uuid/55dd7204-5071-4f61-a113-3892858355c4";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2B98-6AA2";
+    { device = "/dev/disk/by-uuid/082A-DB20";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/abe11a9c-5595-4235-b005-66c914afa080"; }
+    [ { device = "/dev/disk/by-uuid/d834f045-9d53-4014-b569-0dbedcc7ff1e"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -34,6 +34,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
