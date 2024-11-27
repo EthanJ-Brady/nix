69f22fb66ad67584ee857a7fcd1c5a88a5b10004
Ethan Brady <git@ethanbrady.xyz>
2024-11-27 12:32:42 -0700

    Added keymaps and ghost text to nvim-cmp

--- modules/home/nixvim/plugins/lsp.nix
+++ modules/home/nixvim/plugins/lsp.nix
@@ -9,11 +9,22 @@
       cmp = {
         enable = true;
         autoEnableSources = true;
-        settings.sources = [
-          { name = "nvim_lsp"; }
-          { name = "luasnip"; }
-          { name = "path"; }
-        ];
+        settings = {
+          experimental.ghost_text = true;
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-n>" = "cmp.mapping.select_next_item()";
+            "<C-p>" = "cmp.mapping.select_prev_item()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-y>" = "cmp.mapping.confirm({ select = true })";
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+          ];
+        };
       };
       lsp = {
         enable = true;
