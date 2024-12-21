ed9b2ce964f09e09e056c07e16ae41c483a9c49c
Ethan Brady <git@ethanbrady.xyz>
2024-12-21 03:21:17 -0700

    Added a steam module

---
+++ modules/nixos/steam.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  options = {
+    steam.enable = lib.mkEnableOption "Enables steam and opens the associated ports/firewalls";
+  };
+
+  config = lib.mkIf config.steam.enable {
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
+  };
+}
