5a42ae437cf6e3d9b583e5ec4a2d2c0752391313
Ethan Brady <git@ethanbrady.xyz>
2024-12-20 23:59:44 -0700

    Created an nvidia module for nixos based on my previous nixos configuration

---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+{
+  options = {
+    nvidia.enable = lib.mkEnableOption "Enables support for Nvidia graphics card";
+  };
+
+  config = lib.mkIf config.nvidia.enable {
+    services.xserver.videoDrivers = [ "nvidia" ];
+
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      powerManagement.enable = true;
+      prime = {
+        offload.enable = false;
+        sync.enable = true;
+      };
+    };
+  };
+}
