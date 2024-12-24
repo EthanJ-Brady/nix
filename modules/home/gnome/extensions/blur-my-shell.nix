2d173e58cfc5411d713fc8d0b30e0f6ef11690ec
Ethan Brady <git@ethanbrady.xyz>
2024-12-24 00:48:44 -0700

    Added all my gnome extensions from the previous config into the home manager gnome module

---
+++ modules/home/gnome/extensions/blur-my-shell.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf config.gnome.enable {
+    home.packages = with pkgs; [ gnomeExtensions.blur-my-shell ];
+
+    dconf.settings = {
+      "org/gnome/shell".enabled-extensions = [
+        "blur-my-shell@aunetx"
+      ];
+
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        "brightness" = 1.0;
+        "sigma" = "30";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/panel".blur = false;
+
+      "org/gnome/shell/extensions/blur-my-shell/applications" = {
+        "blur" = true;
+        "opacity" = "230";
+        "whitelist" = [
+          "kitty"
+        ];
+      };
+    };
+  };
+}
