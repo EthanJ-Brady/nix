09a11b51326f4006563fd3431b8c6b4ad19fef98
Ethan Brady <git@ethanbrady.xyz>
2024-12-23 23:48:12 -0700

    Added bootloader module to nixos modules

---
+++ modules/nixos/bootloader.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  options = {
+    bootloader.enable = lib.mkEnableOption "Enables boot configuration settings with grub";
+  };
+
+  config = lib.mkIf config.bootloader.enable {
+    boot.loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+        configurationLimit = 10;
+      };
+    }; 
+  };
+}
