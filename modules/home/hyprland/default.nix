7cb5a88d476e02e9d02d626604f088af2aad5cac
Ethan Brady <git@ethanbrady.xyz>
2025-01-12 01:55:20 -0700

    Added keybinds for moving and navigating around windows in hyprland

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -14,11 +14,21 @@
       settings = {
         "$appLauncher" = "ALT_SHIFT_CTRL";
         "$mod" = "ALT";
+        "$mod_alt" = "ALT_SHIFT";
         bind = [
           "$appLauncher, B, exec, vivaldi"
           "$appLauncher, D, exec, vesktop"
           "$appLauncher, T, exec, kitty"
           "$appLauncher, S, exec, steam"
+          "SUPER, Q, killactive"
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
+          "$mod_alt, left, movewindow, l"
+          "$mod_alt, right, movewindow, r"
+          "$mod_alt, up, movewindow, u"
+          "$mod_alt, down, movewindow, d"
         ];
         bindm = [
           "$mod, mouse:272, moveWindow"
