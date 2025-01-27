25782f6a746d364ffe8015a5be69dc4bf2845da0
Ethan Brady <git@ethanbrady.xyz>
2025-01-27 13:44:41 -0700

    Updated Catppuccin tmux to a more recent release
    I had to turn off the rounded style because of an issue with
    transparency (github
    issue)[https://github.com/catppuccin/tmux/issues/403]

--- modules/home/catppuccin/tmux.nix
+++ modules/home/catppuccin/tmux.nix
@@ -6,31 +6,51 @@
 }:
 {
   config = lib.mkIf (config.catppuccin.enable && config.tmux.enable) {
-    programs.tmux.plugins = with pkgs; [
+    programs.tmux.plugins = [
       {
-        plugin = tmuxPlugins.catppuccin;
+        plugin = pkgs.tmuxPlugins.catppuccin.overrideAttrs (oldAttrs: {
+          version = "v2.1.2";
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "tmux";
+            rev = "v2.1.2";
+            sha256 = "sha256-gMBpINeHS+5TCsbJBHhXKEF+fG58FmJrIJoQWYdQqc0=";
+          };
+        });
         extraConfig = ''
-          set -g @catppuccin_flavour "${lib.strings.toLower config.catppuccin.flavor}"
-          set -g @catppuccin_window_left_separator " "
-          set -g @catppuccin_window_right_separator " "
-          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_flavor "${lib.strings.toLower config.catppuccin.flavor}"
+          set -g @catppuccin_status_background "none"
+
           set -g @catppuccin_window_number_position "right"
-
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-
-          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_text "#W"
           set -g @catppuccin_window_current_text "#W"
 
-          set -g @catppuccin_status_modules_right "directory session"
-          set -g @catppuccin_status_left_separator  " "
-          set -g @catppuccin_status_right_separator ""
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-
-          set -g @catppuccin_directory_text "#{pane_current_path}"
+          set -g status-left ""
+          set -g status-right ""
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_directory}"
         '';
       }
     ];
   };
 }
+
+# set -g @catppuccin_flavour "${lib.strings.toLower config.catppuccin.flavor}"
+# set -g @catppuccin_window_left_separator " "
+# set -g @catppuccin_window_right_separator " "
+# set -g @catppuccin_window_middle_separator " █"
+# set -g @catppuccin_window_number_position "right"
+#
+# set -g @catppuccin_window_default_fill "number"
+# set -g @catppuccin_window_default_text "#W"
+#
+# set -g @catppuccin_window_current_fill "number"
+# set -g @catppuccin_window_current_text "#W"
+#
+# set -g @catppuccin_status_modules_right "directory session"
+# set -g @catppuccin_status_left_separator  " "
+# set -g @catppuccin_status_right_separator ""
+# set -g @catppuccin_status_fill "icon"
+# set -g @catppuccin_status_connect_separator "no"
+#
+# set -g @catppuccin_directory_text "#{pane_current_path}"
