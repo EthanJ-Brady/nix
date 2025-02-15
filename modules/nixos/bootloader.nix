3619259de1b7723157ff7203131d94cee8c049be
Ethan Brady <git@ethanbrady.xyz>
2025-02-14 17:34:41 -0700

    Updated the bootloader to show a nixos logo on boot and skip the boot options

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -8,14 +8,37 @@
   };
 
   config = lib.mkIf config.bootloader.enable {
-    boot.loader = {
-      efi.canTouchEfiVariables = true;
-      grub = {
+    boot = {
+      plymouth = {
         enable = true;
-        efiSupport = true;
-        device = "nodev";
-        useOSProber = true;
-        configurationLimit = 10;
+      };
+
+      # Enable "Silent Boot"
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+        "plymouth.force-splash=1"
+        "plymouth.device=all"
+      ];
+
+      loader = {
+        efi.canTouchEfiVariables = true;
+        timeout = 0;
+
+        grub = {
+          enable = true;
+          efiSupport = true;
+          device = "nodev";
+          useOSProber = true;
+          configurationLimit = 10;
+        };
       };
     };
   };
