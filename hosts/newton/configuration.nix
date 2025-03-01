a3cdaf02d714e7d3c1a0786cb009873eb36952ce
Ethan Brady <git@ethanbrady.xyz>
2025-03-01 02:29:36 -0700

    Made some cleaning changes to the newton configuration

--- hosts/newton/configuration.nix
+++ hosts/newton/configuration.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   custom = {
     enable = true;
     desktop.enable = true;
@@ -6,17 +6,8 @@
 
   users.users.ethanbrady.home = "/Users/ethanbrady";
   networking.hostName = "newton";
-
-  environment.systemPackages = with pkgs; [
-    neofetch
-  ];
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
 
-  # Used for backwards compatibility, please read the changelog before changing.
   system.stateVersion = 5;
-
-  # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 }
