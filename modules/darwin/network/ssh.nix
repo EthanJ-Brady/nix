56766b47948f8f0c05a7d582f8b4873ada2b7862
Ethan Brady <git@ethanbrady.xyz>
2025-03-01 02:37:07 -0700

    Updated the darwin ssh module to use the user username instead of redundently defining it otherwise

--- modules/darwin/network/ssh.nix
+++ modules/darwin/network/ssh.nix
@@ -8,15 +8,11 @@
 in {
   options = {
     custom.network.ssh.enable = lib.mkEnableOption "Enables the ssh configuration for darwin";
-    custom.network.ssh.username = lib.mkOption {
-      default = "ethanbrady";
-      type = lib.types.str;
-    };
   };
 
   config = lib.mkIf config.custom.network.ssh.enable {
     services.openssh.enable = true;
-    users.users."${config.custom.network.ssh.username}".openssh.authorizedKeys.keyFiles =
+    users.users."${config.custom.user.username}".openssh.authorizedKeys.keyFiles =
       map (
         file: "${keyDir}/${file}"
       )
