4ebe772bcc53c8d6e07274e2667595d5cbbbf9d3
Ethan Brady <git@ethanbrady.xyz>
2025-03-06 19:14:49 -0700

    Added a keybind for wofi that allows you to select special icons. Will probably move this into a unified wofi solution someday

--- modules/home/wofi.nix
+++ modules/home/wofi.nix
@@ -12,9 +12,17 @@
       enable = true;
     };
 
+    home.file = {
+      ".config/wofi/nerdfont.txt".source = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/8bitmcu/NerdFont-Cheat-Sheet/main/nerdfont.txt";
+        sha256 = "sha256:18rzlify5g7naqqryzkvnsh7c0y8drmqa8x83mra7mxy229a81bn";
+      };
+    };
+
     wayland.windowManager.hyprland.settings.bind = lib.mkIf config.hyprland.enable [
       "SUPER, space, exec, wofi --show drun"
       "SUPER, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
+      "SUPER, E, exec, cat ~/.config/wofi/nerdfont.txt | awk '{print $1, $4}' | wofi --dmenu | awk '{printf \"%s\", $1}' | wl-copy"
     ];
   };
 }
