de352d8975b42fe3d2c08a86418a99c171269f89
Ethan Brady <git@ethanbrady.xyz>
2025-03-23 17:14:45 -0600

    Updated the morse home.nix to use the updated home modules

--- hosts/morse/home.nix
+++ hosts/morse/home.nix
@@ -1,29 +1,16 @@
 {pkgs, ...}: {
-  bat.enable = true;
+  custom = {
+    enable = true;
+  };
   catppuccin.enable = true;
-  eza.enable = true;
-  fzf.enable = true;
-  git.enable = true;
-  nixvim.enable = true;
-  oh-my-posh.enable = true;
-  ssh.enable = true;
-  tmux.enable = true;
-  zettel.enable = true;
-  zoxide.enable = true;
-  zsh.enable = true;
-
-  programs.git.userEmail = "git@ethanbrady.xyz";
-  programs.git.userName = "EthanJ-Brady";
-
-  home.username = "ethan";
-  home.homeDirectory = "/home/ethan";
-
-  home.stateVersion = "23.11";
-
-  programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
     tldr
     neofetch
   ];
+
+  home.username = "ethan";
+  home.homeDirectory = "/home/ethan";
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
 }
