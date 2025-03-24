ca685c6edaf581433228ecc8cc8c211e3214abe0
mohs <git@ethanbrady.xyz>
2025-03-23 22:56:21 -0600

    Updated the mohs home.nix to match the new interface

--- hosts/mohs/home.nix
+++ hosts/mohs/home.nix
@@ -1,29 +1,11 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  bat.enable = true;
-  bun.enable = true;
+{...}: {
+  custom = {
+    enable = true;
+  };
   catppuccin.enable = true;
-  eza.enable = true;
-  fzf.enable = true;
-  git.enable = true;
-  ghostty.enable = true;
-  nixvim.enable = true;
-  oh-my-posh.enable = true;
-  ssh.enable = true;
-  tmux.enable = true;
-  zettel.enable = true;
-  zoxide.enable = true;
-  zsh.enable = true;
-
-  programs.git.userEmail = "git@ethanbrady.xyz";
-  programs.git.userName = "EthanJ-Brady";
 
   home.username = "mohs";
   home.homeDirectory = "/home/mohs";
-
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 }
