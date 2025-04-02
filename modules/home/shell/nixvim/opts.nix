db596a36a0c62e3b8d2fae69a1475189facc3ff3
Ethan Brady <git@ethanbrady.xyz>
2025-04-02 01:59:40 -0600

    Added winborder option to my nixvim configuration

--- modules/home/shell/nixvim/opts.nix
+++ modules/home/shell/nixvim/opts.nix
@@ -36,6 +36,7 @@
       tabstop = 2; # Define the width of a tab character as 2 spaces
       timeoutlen = 400; # Set the time (in milliseconds) to wait for a mapped sequence to complete
       undofile = true; # Persist undo history between editing sessions
+      winborder = "rounded"; # Use rounded borders for the floating windows
     };
   };
 }
